cea96790136c1c97509760b37b06d6b75cccba1f
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-11-03 23:56:11 -0300

    up

---
+++ text/src/main/java/com/mycompany/text/Text.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.text;
+
+/**
+ *
+ * @author gilma
+ */
+public class Text {
+
+    public static void main(String[] args) {
+        int x = 4;
+        do {
+            x--;
+        } while (x > 5);
+        
+        System.out.println(x);
+    }
+}
