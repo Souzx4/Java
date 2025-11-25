68a40f107403a3dc9fd12e962b7297efd77ab796
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-11-25 13:37:07 -0300

    up

---
+++ TiposPrimitivos/src/main/java/com/mycompany/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tiposprimitivos;
+
+/**
+ *
+ * @author Eletel_Monitor
+ */
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        float nota = 8.5f;
+        System.out.println("Sua nota e " + nota);
+        System.out.print("Sua nota e " + nota);
+        System.out.printf("A nota e %.2f \n", nota);
+    }
+}
