9050ce1bf7259999da36a762bfc58715ee6f59a1
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-11-26 12:03:20 -0300

    ok

---
+++ ContaBancaria/src/main/java/com/mycompany/contabancaria/ContaBancaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.contabancaria;
+
+/**
+ *
+ * @author Eletel_Monitor
+ */
+public class ContaBancaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
