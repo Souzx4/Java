014159848028a1850c64b73ddd8f7569b83c7ac4
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-11-27 08:07:33 -0300

    up

--- ContaBancaria/src/main/java/com/mycompany/contabancaria/ContaBanco.java
+++ ContaBancaria/src/main/java/com/mycompany/contabancaria/ContaBanco.java
@@ -23,10 +23,15 @@ public class ContaBanco {
             System.out.println("Conta não pode ser fechada, pois ainda tem dinheiro");
         } else if (this.getSaldo() < 0){
             System.out.println("Conta não pode ser fechada pois tem débito");
+        } else {
+            this.setStatus(false);
+            System.out.println("Conta fechada com sucesso!!");
         }
     }
     public void depositar(){
-        
+        if(this.getStatus()) {
+            
+        }
     }
     public void sacar(){
         
